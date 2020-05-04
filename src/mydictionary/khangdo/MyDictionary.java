dcd9e08b40dde2ec4bf4af642f6e0f71b4739864
Admin <Admin@Admin>
2020-05-04 00:46:39 -0400

    Merge origin/ProgrammingCode
    Conflicts:
    	nbproject/project.properties

--- src/mydictionary/khangdo/MyDictionary.java
+++ src/mydictionary/khangdo/MyDictionary.java
@@ -5,17 +5,34 @@
  */
 package mydictionary.khangdo;
 
-/**
- *
- * @author Admin
- */
-public class MyDictionary {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ * A Program to store and review your own vocabulary
+ * @author Khang Do, 2020
+ */
+public class MyDictionary extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDisplayVocab.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("My Dictionary");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
     }
     
 }
