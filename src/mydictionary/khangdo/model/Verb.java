b94053b388658d1f2fdcfd457e92281ca95d6cc3
Admin <Admin@Admin>
2020-06-09 00:06:26 -0400

    Verb class

--- src/mydictionary/khangdo/model/Verb.java
+++ src/mydictionary/khangdo/model/Verb.java
@@ -10,7 +10,55 @@ package mydictionary.khangdo.model;
  * @author Khang Do, 2020
  */
 public class Verb extends Word{
-    private Verb[] pastTense;
+    private Verb[] pastTense = new Verb[3]; //
     private Verb presentParticiple;
-    private Verb[] simpleTense;
+    private Verb[] simpleTense = new Verb[2]; //I|they & he|she|it
+    
+    public Verb() {
+        super();
+    }
+
+    /**
+     * @return the pastTense
+     */
+    public Verb[] getPastTense() {
+        return pastTense;
+    }
+
+    /**
+     * @param pastTense the pastTense to set
+     */
+    public void setPastTense(Verb[] pastTense) {
+        this.pastTense = pastTense;
+    }
+
+    /**
+     * @return the presentParticiple
+     */
+    public Verb getPresentParticiple() {
+        return presentParticiple;
+    }
+
+    /**
+     * @param presentParticiple the presentParticiple to set
+     */
+    public void setPresentParticiple(Verb presentParticiple) {
+        this.presentParticiple = presentParticiple;
+    }
+
+    /**
+     * @return the simpleTense
+     */
+    public Verb[] getSimpleTense() {
+        return simpleTense;
+    }
+
+    /**
+     * @param simpleTense the simpleTense to set
+     */
+    public void setSimpleTense(Verb[] simpleTense) {
+        this.simpleTense = simpleTense;
+    }
+    
+    
 }
