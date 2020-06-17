aca8c204c33ac1d1c7f9235cac471942e45b11b7
Admin <Admin@Admin>
2020-06-16 23:23:34 -0400

    Model class

---
+++ src/mydictionary/khangdo/model/WordHandler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydictionary.khangdo.model;
+
+import java.util.ArrayList;
+
+/**
+ * A class handle all methods of words and groups of words
+ * @author Khang Do
+ */
+public class WordHandler {
+    private ArrayList<Word> groupWords;
+    
+    public WordHandler() {
+        
+    }
+}
