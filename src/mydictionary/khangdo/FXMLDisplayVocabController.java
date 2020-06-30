d415db0f3a593bc7fb3bbb000da9ae6c25bfdbad
Admin <Admin@Admin>
2020-06-30 00:56:28 -0400

    Display Vocab - Create a new word

--- src/mydictionary/khangdo/FXMLDisplayVocabController.java
+++ src/mydictionary/khangdo/FXMLDisplayVocabController.java
@@ -17,6 +17,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import mydictionary.khangdo.model.Definition;
+import mydictionary.khangdo.model.Example;
+import mydictionary.khangdo.model.Noun;
+import mydictionary.khangdo.model.Pronunciation;
+import mydictionary.khangdo.model.Verb;
+import mydictionary.khangdo.model.Word;
 
 /**
  * FXML Controller class
@@ -30,6 +36,41 @@ public class FXMLDisplayVocabController implements Initializable {
 
     @FXML
     private Label lblSynonymWord;
+
+    private ArrayList<Word> wordList = new ArrayList<>();
+
+    public Word setANewWord(String[] fields) {
+        Pronunciation pron = new Pronunciation(fields[2]);
+
+        Definition meaning = new Definition();
+        meaning.setMeaning(fields[5]);
+
+        ArrayList<String> collocation = new ArrayList<String>();
+        collocation.add(fields[4]);
+        meaning.setCollocation(collocation);
+
+        ArrayList<Example> ex = new ArrayList<Example>();
+        ArrayList<String> instance = new ArrayList<String>();
+        instance.add(fields[8]);
+
+        ex.add(new Example(instance));
+        ArrayList<Definition> meanings = new ArrayList<Definition>();
+        meanings.add(meaning);
+
+        switch (fields[3]) {
+            default:
+            case "Noun":
+                Noun noun = new Noun(fields[1], pron);
+                noun.setMeanings(meanings);
+                return noun;
+//                break;
+            case "Verb":
+                Verb verb = new Verb(fields[1], pron);
+                verb.setMeanings(meanings);
+                return verb;
+            
+        }
+    }
 
     /**
      * Initializes the controller class.
@@ -46,15 +87,8 @@ public class FXMLDisplayVocabController implements Initializable {
                 String print = "";
                 while ((line = br.readLine()) != null) {
                     String[] fields = line.split(",", -1);
-                    for (String list: fields) {
-                        System.out.println(list);
-                        print += (list + ".\n");
-                    }
-//                    Scanner scLine = new Scanner(sc.nextLine());
-//                    scLine.useDelimiter("||");
-//                    while (scLine.hasNext()) {
-//                        System.out.println(scLine.next());
-//                    }
+
+                    print = setANewWord(fields).toString();
                 }
                 txtExample.setText(print);
             } catch (FileNotFoundException ex) {
